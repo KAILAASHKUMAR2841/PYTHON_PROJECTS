71afed2035472065b74b834ab60ca5e942d4ef57
KAILAASH KUMAR P V <62185391+KAILAASHKUMAR2841@users.noreply.github.com>
2023-01-24 11:58:40 +0530

    Update guess_the_number_cpu.py

--- Guess the number (cpu)/guess_the_number_cpu.py
+++ Guess the number (cpu)/guess_the_number_cpu.py
@@ -10,20 +10,5 @@ def guessing(x):
       print("Too Low. Try Again! :(")
   print(f"Hurray! You guessed the Correct_Number :) '{guess}'")
 
-guessing(10)
-'''def computer_guess(x):
-  low = 1
-  high = x
-  feedback = ''
-  while feedback != 'c':
-    if low != high:
-      guess = random.randint(low, high)
-    else:
-      guess = low
-    feedback = input(f"Is {guess} too High(H), too Low(L), or Correct(C)").lower()
-    if feedback == 'h':
-      high = guess - 1
-    elif feedback == 'l':
-      low = guess + 1
-  print(f"Yay! computer guessed the number correctly")
-computer_guess(10)'''
+x = int(input("Enter the range of number to be guessed: "))
+guessing(x)

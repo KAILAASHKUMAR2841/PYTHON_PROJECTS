6a506601c19b856c6b8b6a4ba1ff41bd770b33fd
KAILAASHKUMAR2841 <pv.kailaashkumar@gmail.com>
2024-04-03 11:51:59 +0530

    numpy edit

--- Guess the number (cpu)/guess_the_number_cpu.py
+++ Guess the number (cpu)/guess_the_number_cpu.py
@@ -1,14 +1,18 @@
+import numpy as np
 import random
+
+
 def guessing(x):
-  random_number = random.randint(1, x)
-  guess = 0
-  while guess != random_number:
-    guess = int(input("Guess the number: "))
-    if (guess > random_number):
-      print("Too High. Try again! :(")
-    elif (guess < random_number):
-      print("Too Low. Try Again! :(")
-  print(f"Hurray! You guessed the Correct_Number :) '{guess}'")
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input("Guess the number: "))
+        if (guess > random_number):
+            print("Too High. Try again! :(")
+        elif (guess < random_number):
+            print("Too Low. Try Again! :(")
+    print(f"Hurray! You guessed the Correct_Number :) '{guess}'")
+
 
 x = int(input("Enter the range of number to be guessed: "))
 guessing(x)
